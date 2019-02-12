7b1366a63bf1e3ba4d35ecd5c656714dbc2ac2b2
Abhay <abhayprince@outlook.com>
2019-02-12 10:14:25 +0530

    exports to excel

---
+++ TogoFogo/Controllers/ClientDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TogoFogo.Models;
+
+namespace TogoFogo.Controllers
+{
+    public class ClientDataController : Controller
+    {
+        // GET: ClientData
+        //public ActionResult Index()
+        //{
+
+        //    return View();
+        //}
+        public async Task<ActionResult> Create()
+        {
+            var clientDate = new ClientDataModel();
+            clientDate.ClientList = new SelectList(await CommonModel.GetClientData(), "Name", "Text");
+            clientDate.ServiceTypeList = new SelectList(await CommonModel.GetServiceType(), "Value", "Text");
+            return View(clientDate);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Upload(ClientDataModel clientDataModel)
+        {
+            return View(clientDataModel);
+        }
+    }
+}
