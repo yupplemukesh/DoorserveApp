9a5647d8586a4e54cf7cc89f770ebe2db85d7a7a
Admin <Admin@USER>
2019-02-13 10:39:38 +0530

    new add user section

---
+++ TogoFogo/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TogoFogo.Models
+{
+    public class User
+    {
+        public Int64 UserId { get; set; }
+        public  string UserName { get; set; }
+        public string Password { get; set; }
+        public Boolean IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifyBy { get; set; }
+        public string CreatedDate { get; set; }
+        public string ModifyDate { get; set; }
+
+    }
+}
