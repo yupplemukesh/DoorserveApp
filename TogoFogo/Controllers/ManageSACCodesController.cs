d7b2820bb7c4176b7fff7b38759467a06f41e8f8
Net <Net@ADMIN-PC>
2019-02-18 19:29:45 +0530

    manage sac code

--- TogoFogo/Controllers/ManageSACCodesController.cs
+++ TogoFogo/Controllers/ManageSACCodesController.cs
@@ -16,15 +16,22 @@ namespace TogoFogo.Controllers
         private readonly string _connectionString =
             ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         DropdownBindController dropdown = new DropdownBindController();
+      
         // GET: ManageSACCodes
         public ActionResult SacCodes()
         {
-            ViewBag.CountryId = new SelectList(Enumerable.Empty<SelectList>());
+            /*ViewBag.CountryId = new SelectList(Enumerable.Empty<SelectList>());
             ViewBag.StateId = new SelectList(Enumerable.Empty<SelectList>());
             ViewBag.GstCategoryId = new SelectList(Enumerable.Empty<SelectList>());
             ViewBag.Gst_HSNCode_Id = new SelectList(Enumerable.Empty<SelectList>());
             ViewBag.CTH_NumberId = new SelectList(Enumerable.Empty<SelectList>());
             ViewBag.SACId = new SelectList(Enumerable.Empty<SelectList>());
+
+            sm.CountryList = new SelectList(dropdown.BindCountry(), "Value", "Text");
+            sm.StateList = new SelectList(dropdown.BindState(), "Value", "Text");
+            sm.GstList = new SelectList(dropdown.BindGst(), "Value", "Text");
+            sm.GstHsnCodeList = new SelectList(dropdown.BindGstHsnCode(), "Value", "Text");*/
+
             if (TempData["Message"] != null)
             {
                 ViewBag.Message = TempData["Message"].ToString();
@@ -34,11 +41,13 @@ namespace TogoFogo.Controllers
         }
         public ActionResult AddSacCodes()
         {
-            ViewBag.CountryId = new SelectList(dropdown.BindCountry(), "Value", "Text");
-            ViewBag.StateId = new SelectList(Enumerable.Empty<SelectList>());
-            ViewBag.GstCategoryId = new SelectList(dropdown.BindGst(), "Value", "Text");
-           
-            return View();
+            SacCodesModel sm = new SacCodesModel();
+            
+            sm.CountryList = new SelectList(dropdown.BindCountry(), "Value", "Text");
+            sm.StateList = new SelectList(Enumerable.Empty<SelectList>());
+            sm.GstList = new SelectList(dropdown.BindGst(), "Value", "Text");
+
+            return View(sm);
         }
         [HttpPost]
         public ActionResult AddSacCodes(SacCodesModel model)
@@ -100,21 +109,23 @@ namespace TogoFogo.Controllers
             using (var con = new SqlConnection(_connectionString))
             {
                 //var result = con.Query<SacCodesModel>("Select * from MstSacCodes", new { }, commandType: CommandType.Text).ToList();
-                var result= con.Query < SacCodesModel >("select m.Applicable_Tax_Type,m.GstCategoryid,m.GstHeading,m.Gst_HSN_Code,m.CTH_Number,m.SAC,m.Product_Sale_Range, m.CGST, m.SGST_UTGST, m.IGST, m.GstProductCat, m.GstProductSubCat, m.Description_Of_Goods,m.IsActive, m.Comments, m.CreatedDate, m.ModifyDate, u.Username Cby, u1.Username Mby from MstSacCodes m join create_User_Master u on u.id = m.CreatedBy left outer join create_User_Master u1 on m.ModifyBy = u1.id ", new { }, commandType: CommandType.Text).ToList();
+                var result= con.Query < SacCodesModel >("select c.Cnty_Name,s.St_Name,m.SacCodesId, m.Applicable_Tax_Type,m.GstCategoryid,m.GstHeading,m.Gst_HSN_Code,m.CTH_Number,m.SAC,m.Product_Sale_Range, m.CGST, m.SGST_UTGST, m.IGST, m.GstProductCat, m.GstProductSubCat, m.Description_Of_Goods, m.IsActive, m.Comments, m.CreatedDate, m.ModifyDate, u.Username Cby,u1.Username Mby from MstSacCodes m join create_User_Master u on u.id = m.CreatedBy left outer join create_User_Master u1 on m.ModifyBy = u1.id left outer join MstCountry c on c.Cnty_ID = m.CountryId left outer join mststate s on s.St_ID = m.StateId", new { }, commandType: CommandType.Text).ToList();
                 return View(result);
             }
         }
 
         public ActionResult EditSacCode(int sacCodeId)
         {
-            ViewBag.CountryId = new SelectList(dropdown.BindCountry(), "Value", "Text");
-            ViewBag.StateId = new SelectList(dropdown.BindState(), "Value", "Text");
-            ViewBag.GstCategoryId = new SelectList(dropdown.BindGst(), "Value", "Text");
+          
+
+         
             using (var con = new SqlConnection(_connectionString))
             {
                 var result = con.Query<SacCodesModel>("Select * from MstSacCodes Where SacCodesId=@SacCodesId", new { @SacCodesId = sacCodeId },
                     commandType: CommandType.Text).FirstOrDefault();
-                
+                result.CountryList = new SelectList(dropdown.BindCountry(), "Value", "Text");
+                result.StateList = new SelectList(dropdown.BindState(result.CountryId), "Value", "Text");
+                result.GstList = new SelectList(dropdown.BindGst(), "Value", "Text");
                 return View(result);
             }
         }
@@ -160,19 +171,28 @@ namespace TogoFogo.Controllers
                         {
                             TempData["Message"] = "Not Updated Successfully";
                         }
+                    }
+
+
+                    return RedirectToAction("SacCodes");
+                 
                 }
 
+                else
+                {
+                    model.CountryList = new SelectList(dropdown.BindCountry(), "Value", "Text");
+                    model.StateList = new SelectList(dropdown.BindState(model.CountryId), "Value", "Text");
+                    model.GstList = new SelectList(dropdown.BindGst(), "Value", "Text");
                     return View(model);
+
                 }
-
-
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
-            return RedirectToAction("SacCodes");
+            
         }
     }
 }
