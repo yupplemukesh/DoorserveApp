8200bf8a46a93666652f0c8a36aa5b1a54e08085
Net <Net@ADMIN-PC>
2019-06-13 13:24:11 +0530

    Bind City State Country By Pincode in ContactPerson

--- TogoFogo/Repository/ContactPersons/ContactPerson.cs
+++ TogoFogo/Repository/ContactPersons/ContactPerson.cs
@@ -31,7 +31,7 @@ namespace TogoFogo.Repository
             sp.Add(param);
             param = new SqlParameter("@CONSTATEID", ToDBNull(contact.StateId));
             sp.Add(param);
-            param = new SqlParameter("@CONCITYID", ToDBNull(contact.CityId));
+            param = new SqlParameter("@CONCITY", ToDBNull(contact.City));
             sp.Add(param);
             param = new SqlParameter("@CONADDRESS", ToDBNull(contact.Address));
             sp.Add(param);
@@ -75,7 +75,7 @@ namespace TogoFogo.Repository
             sp.Add(param);
             param = new SqlParameter("@IsSingleCenter", ToDBNull(contact.IsSingleCenter));
             sp.Add(param);
-            var sql = "USPADDOREDITCONTACTS @CONTACTID,@REFKEY,@CONADDRESSTYPEID,@CONCOUNTRYID,@CONSTATEID,@CONCITYID,@CONADDRESS,@CONLOCALITY ,@CONNEARBYLOCATION,@CONPIN," +
+            var sql = "USPADDOREDITCONTACTS @CONTACTID,@REFKEY,@CONADDRESSTYPEID,@CONCOUNTRYID,@CONSTATEID,@CONCITY,@CONADDRESS,@CONLOCALITY ,@CONNEARBYLOCATION,@CONPIN," +
                 "@CONFNAME,@CONLNAME,@CONNUMBER,@CONEMAIL,@CONPANNUMBER,@CONPANFILENAME,@CONVOTERID,@CONVOTERIDFILENAME,@CONADHAARNUMBER,@CONADHAARFILENAME,@ACTION,@USER,@ISUSER,@USERTYPEID, @DefaultPWD,@CompId,@IsSingleCenter";
             var res = await _context.Database.SqlQuery<ResponseModel>(sql, sp.ToArray()).FirstOrDefaultAsync();
             if (res.ResponseCode==0)
