ca8672849fe6f7a7fcd3d7528dcc2d53e9a2b9d4
Vikas <vikas.agarwal@yuppletech.com>
2019-09-13 18:38:23 +0530

    fixes

--- doorserve/Models/Services/ServiceModel.cs
+++ doorserve/Models/Services/ServiceModel.cs
@@ -11,39 +11,44 @@ namespace doorserve.Models
     public class ServiceModel:RegistrationModel
     {
         public Guid? ServiceId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter Category Name")]
         [DisplayName("Category Name")]
         public int CategoryId { get; set; }
         public string Category { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter Sub-Category Name")]
         [DisplayName("Sub-Category Name")]
         public int SubCategoryId { get; set; }
         public string SubCategory { get; set; }
         public int CountPin { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please select Service Type")]
         [DisplayName("Service Type")]
         public int ServiceTypeId { get; set; }
         public string ServiceType { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please select Service Delivery Type")]
         [DisplayName("Service Delivery Type")]
         public int DeliveryTypeId{ get; set; }
         public string DeliveryType { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter Amount")]
         [Range(0, double.MaxValue, ErrorMessage = "Please enter Amount")]
         public decimal? ServiceCharges { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter Amount")]
         [Range(0, double.MaxValue, ErrorMessage = "Please enter Amount")]
         public decimal? MApprovalCost { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter Warranty")]
         [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter number only")]
         public int? WarranyPeriod { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter TAT(Hours)")]
         [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Enter number only")]
         public int? TargetApprovalTime { get; set; }
+
+
         public Guid? RefKey { get; set; }
         public SelectList SupportedCategoryList { get; set; }
         public SelectList SupportedSubCategoryList { get; set; }      
